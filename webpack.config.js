0b95621674b0e5d60a83986d70810a56fbbb6d1c
Cuneyt Aliustaoglu <aliustaoglu@yahoo.com>
2018-06-27 20:29:15 +1200

    Webpack sourcemap and absolute imports

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
 const webpack = require('webpack');
+const path = require('path');
 
 module.exports = {
   entry: ['react-hot-loader/patch', './src/index.js'],
+  devtool: 'eval-source-map',
   module: {
     rules: [
       {
@@ -42,14 +44,23 @@ module.exports = {
     ]
   },
   resolve: {
-    extensions: ['*', '.js', '.jsx']
+    extensions: ['*', '.js', '.jsx'],
+    modules: [path.resolve(__dirname, './src'), 'node_modules'],
+    alias: {
+      reducers: path.resolve(__dirname, './src')
+    }
   },
   output: {
     path: __dirname + '/dist',
     publicPath: '/',
     filename: 'bundle.js'
   },
-  plugins: [new webpack.HotModuleReplacementPlugin()],
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.SourceMapDevToolPlugin({
+      filename: '[name].js.map'
+    })
+  ],
   devServer: {
     contentBase: './dist',
     hot: true
